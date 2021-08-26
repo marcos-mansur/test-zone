585fcb4e64e82959e9a84bc17665aaa1dc73335c
marcos-mansur <ticomansur@gmail.com>
2021-08-26 17:51:29 -0300

    parametrize fixture (#4)

--- test_my_module_fixture.py
+++ test_my_module_fixture.py
@@ -1,8 +1,16 @@
 from my_module import square
 
-def test_square_int(input_value):
+import pytest
+
+#convencionally, fixtures are defines in conftest.py, but you can do it in the code too
+@pytest.mark.parametrize(           #passes multiple values
+    'para_inputs', [                #give it a name, "para_inputs"
+    1,5,3,6,8]
+)
+
+def test_square_int(para_inputs):
     #When
-    subject = square(input_value)
+    subject = square(para_inputs)
 
     #Than
     assert isinstance(subject, int)
